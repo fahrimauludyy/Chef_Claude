c6ed7943d4975a5134de5106a2005f97a77fe82a
Fahri Mauludy <idnmuhammadfahri2008@gmail.com>
2026-04-15 23:06:14 +0700

    update chef-claude

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 
 export default function Main() {
     return(
-    <main className=" flex justify-center px-60 pt-12 overflow-x-hidden">
-        <form className="flex flex-col gap-5 w-1/2">
-            <input className="border rounded-md p-3" type="text" placeholder="e.g oregano" aria-label="Add Ingredient" />
-            <button className="bg-[#202020] text-white p-3 rounded-md font-semibold "> + Add Ingredient</button>
+    <main className="w-full flex flex-col items-center pt-16">
+        <h1 className="text-5xl font-bold mb-10">Let's cook your Meal!</h1>
+        <form className="flex flex-row gap-5 w-1/2  justify-center">
+            <input className="border border-gray-300 outline-none focus:border-gray-500 focus:border-2 w-120 rounded-md px-2" type="text" placeholder="e.g oregano" aria-label="Add Ingredient" />
+            <button className="bg-[#202020] text-white px-2 h-12  rounded-md font-semibold "> + Add Ingredient</button>
         </form>
     </main>
     )
